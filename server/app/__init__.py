6e9e35ac261f5a8a1ae01aad50a45e70fb03169b
nimrashakoor <nimrashakoor@outlook.com>
2021-08-05 23:53:58 -0400

    database initialized

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 class books(db.Model):
     __tablename__ = "books"
 
-    _id = db.Column("id", db.Integer(), primary_key=True)
+    _id = db.Column(db.Integer(), primary_key=True)
     isbn = db.Column(db.String())
     playlist_id = db.Column(db.String())
 
@@ -21,13 +21,17 @@ class books(db.Model):
         self.playlist_id = playlist_id
 
     def __repr__(self):
-        return f"<Book {self.isbn}>"
+        # return f"<Book {self.isbn}>"
+        return "<books %r>" % self.isbn
+
+    def __str__(self):
+        return self.isbn
 
 
 class playlists(db.Model):
     __tablename__ = "playlists"
 
-    _id = db.Column("id", db.Integer(), primary_key=True)
+    _id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String())
     description = db.Column(db.String())
 
@@ -38,27 +42,35 @@ class playlists(db.Model):
     def __repr__(self):
         return f"<Playlist {self.name}>"
 
+    def __str__(self):
+        return self.name
+
 
 @app.route("/")
 def index():
     # adding dummy data
     testPlaylist = playlists("Introduction to American Literature", "sample desc")
-    testBook = books("9780399128967", "1")
+    testBook = books("9780399128967", "32")
+    testBook1 = books("9780399128968", "33")
     db.session.add(testBook)
+    db.session.add(testBook1)
     db.session.add(testPlaylist)
     db.session.commit()
 
     # book = books.query.filter_by(isbn="9780399128967").first();
     # file = json.load(open("playlist.json"))
     # print(str(db))
-    return str(playlists.query.all())
+    # return repr(books.query.all())
+    # return str(db.Table("books", db.MetaData()).columns.keys())
+    print(books.query.all())
+    return repr(books.query.all())
 
 
 # @app.route("/<playlist>")
 # def open_file(playlist):
 #     file = json.load(open(playlist + ".json"))
 #     return file
-
+# return playlists.query.filter_by(name=playlist)
 
 if __name__ == "__main__":
     db.drop_all()
