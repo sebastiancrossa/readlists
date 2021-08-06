fe45f4c729511cc7350b1e6d60b89ddfb95a141a
Zuleinis <zuleinis.ramos@upr.edu>
2021-08-05 20:55:10 -0400

    html for navbar and feature card

---
+++ client/pages/playlist-horizontal.jsx
@@ -0,0 +1,17 @@
+// import { Button } from "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Playlists() {
+  return (
+    <div class="row">
+      <div class="row">
+        <div class="col-md-10">
+          <h2>Playlist Name</h2>
+        </div>
+        <div class="col-md-2">
+          <Button>See More</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
