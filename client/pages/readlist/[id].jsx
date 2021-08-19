3d811cd1dda298b34825608aeee1701213f30fc7
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-08-18 23:42:11 -0500

    feat: dynamic content on readlist routes

--- client/pages/readlist/[id].jsx
+++ client/pages/readlist/[id].jsx
@@ -1,12 +1,44 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import { BsPlus } from "react-icons/bs";
 import Nav from "../../components/Nav";
 
 const Readlist = ({ data, playlistData }) => {
+  const [books, setBooks] = useState([]);
+
   const router = useRouter();
   const { id } = router.query;
+
+  useEffect(() => {
+    // Fetching all of the individual book info with the google books api
+    const fetchData = async () => {
+      Promise.all(
+        data.map(async (book) => {
+          const bookData = await fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=isbn:${book.isbn}&key=${process.env.NEXT_PUBLIC_BOOKS_API}`
+          ).then((res) => res.json());
+
+          const fetchedBook = bookData.items[0].volumeInfo;
+
+          setBooks((prevState) => [
+            ...prevState,
+            {
+              title: fetchedBook["title"],
+              authors: fetchedBook["authors"],
+              publishedDate: fetchedBook["publishedDate"],
+              rating: fetchedBook["averageRating"],
+              thumbnail: fetchedBook["imageLinks"]["thumbnail"],
+              link: fetchedBook["infoLink"],
+            },
+          ]);
+        })
+      );
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -20,49 +52,30 @@ const Readlist = ({ data, playlistData }) => {
           </div>
         </div>
       </div>
-      <div>
-        <div class="col-md-10 center">
-          <div class="row">
-            <div class="col-md-2">
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780525559474_p0_v6%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                class="rounded mx-auto d-block"
-                width="auto"
-                height="200"
-              ></img>
-            </div>
-            <div class="col-md-4">
-              <p class="book-title">The Midnight Library</p>
-              <p class="book-author">Matt Haig</p>
-              <p class="book-title">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-            <div class="col-md-2">
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780525559474_p0_v6%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                class="rounded mx-auto d-block"
-                width="auto"
-                height="200"
-              ></img>
-            </div>
-            <div class="col-md-4">
-              <p class="book-title">The Midnight Library</p>
-              <p class="book-author">Matt Haig</p>
-              <p class="book-title">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-            <div className="col-md-4">
-              <AddButton>
-                <BsPlus />
-                <p>Have a book suggestion? Add it to readlist</p>
-              </AddButton>
-            </div>
-          </div>
-        </div>
+      <div class="col-md-10 center">
+        <BooksGrid>
+          {books.map((book) => (
+            <BookItem>
+              <div className="left">
+                <img src={book.thumbnail} alt={book.title} />
+              </div>
+              <div className="right">
+                <h2>{book.title}</h2>
+                <h3>by {book.authors[0]}</h3>
+
+                <p>{book.rating} / 5</p>
+                <p>{book.publishedDate}</p>
+
+                <a href={book.link}>Link to buy</a>
+              </div>
+            </BookItem>
+          ))}
+
+          <AddButton>
+            <p>Suggest a book</p>
+            <BsPlus size={30} />
+          </AddButton>
+        </BooksGrid>
       </div>
     </div>
   );
@@ -92,14 +105,40 @@ export async function getServerSideProps(ctx) {
 }
 
 const AddButton = styled.button`
-  margin: 2rem;
-  padding: 1rem;
-
-  width: 9rem;
-  height: 15rem;
+  width: 8rem;
+  height: 11.18rem;
 
   border: 2px dashed black;
   border-radius: 5px;
+`;
+
+const BooksGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1rem;
+`;
+
+const BookItem = styled.div`
+  display: grid;
+  grid-template-columns: max-content auto;
+  grid-gap: 1rem;
+
+  img {
+    border-radius: 5px;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin: 0;
+  }
+
+  h3 {
+    font-size: 1rem;
+  }
+
+  .right {
+    text-align: left;
+  }
 `;
 
 export default Readlist;
